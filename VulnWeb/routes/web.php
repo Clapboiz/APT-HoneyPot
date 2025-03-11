2e342353358fa24f1e1c53d51c518d209fe2a89a
Clapboiz <phamconglap03@gmail.com>
2025-03-11 21:27:27 +0700

    full apt honeypot

---
+++ VulnWeb/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+// use Symfony\Component\Process\Process;
+// use App\Http\Controllers\CveController;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/env', function (Request $request) {
+
+    if (App::environment('staging')) {
+        Config::set('app.debug', true); // Turn on debug mode
+        return response()->json([
+            'test' => phpinfo(),
+            'environment' => App::environment(),
+            'debug_mode' => config('app.debug'),
+        ]);
+    } else {
+        dd(App::environment());
+    }
+});
